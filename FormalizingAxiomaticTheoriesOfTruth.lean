0671026562a80403cdf3f768e695940f5467d939
ppls-nd-prs <bramswaanen@gmail.com>
2024-12-13 14:51:09 +0100

    Modified auxilarry files

--- FormalizingAxiomaticTheoriesOfTruth.lean
+++ FormalizingAxiomaticTheoriesOfTruth.lean
@@ -1,3 +1,1 @@
--- This module serves as the root of the `FormalizingAxiomaticTheoriesOfTruth` library.
--- Import modules here that should be built as part of the library.
-import «FormalizingAxiomaticTheoriesOfTruth».Basic
+import FormalizingAxiomaticTheoriesOfTruth.Basic
