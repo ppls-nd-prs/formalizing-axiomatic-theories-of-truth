666b3e008f94f349afc7ab5c499e71d99390ecd8
ppls-nd-prs <bramswaanen@gmail.com>
2024-12-13 12:43:02 +0100

    Modified Basic.lean: first try at a definition of LPA

--- FormalizingAxiomaticTheoriesOfTruth/Basic.lean
+++ FormalizingAxiomaticTheoriesOfTruth/Basic.lean
@@ -1,1 +1,18 @@
-def hello := "world"
+import Foundation.Logic.Predicate.Language
+
+open LO
+open FirstOrder
+open Language
+
+inductive PA_Func : ℕ → Type where
+  | zero : PA_Func 0
+  | succ : PA_Func 1
+  | add : PA_Func 2
+  | mult : PA_Func 2
+
+inductive PA_Rel : ℕ → Type where
+  | eq : PA_Rel 2
+
+def lpa : Language where
+  Func := PA_Func
+  Rel := PA_Rel
