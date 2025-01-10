9292732903ada8cdb8dfb6e5ff7ea7eeaf7af3e9
ppls-nd-prs <bramswaanen@gmail.com>
2025-01-10 12:00:55 +0100

    Modified ExamplesAndScratch: turned instance LPA_null to def LPA_null.

--- FormalizingAxiomaticTheoriesOfTruth/ExamplesAndScratch.lean
+++ FormalizingAxiomaticTheoriesOfTruth/ExamplesAndScratch.lean
@@ -6,7 +6,7 @@ open FirstOrder
 
 -- Constructing and printing some terms
 -- Definition of useful LPA terms
-instance LPA_null : SyntacticTerm LPA := Semiterm.func LPA_Func.zero ![]
+def LPA_null : SyntacticTerm LPA := Semiterm.func LPA_Func.zero ![]
 
 def LPA_numeral : ℕ → SyntacticTerm LPA
   | .zero => Semiterm.func LPA_Func.zero ![]
