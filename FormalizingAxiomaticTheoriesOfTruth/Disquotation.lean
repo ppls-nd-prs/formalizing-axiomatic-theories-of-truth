6c21b7c8377f07800660ec0802d8f161e49cc59f
ppls-nd-prs <bramswaanen@gmail.com>
2025-02-24 17:14:07 +0100

    Modified Disquotation.lean: extended der_to_disjunct_list with root case.

--- FormalizingAxiomaticTheoriesOfTruth/Disquotation.lean
+++ FormalizingAxiomaticTheoriesOfTruth/Disquotation.lean
@@ -13,19 +13,93 @@ notation:25 "⌜" φ:25 "⌝" => numeral (Semiformula.toNat φ)
 
 def disquotation_schema (φ : SyntacticFormula signature) : SyntacticFormula signature :=
   (T ![⌜φ⌝]) ⇔  φ
-def disquotation_set (Γ : SyntacticFormula signature → Prop) : Theory signature :=
+def disquotation_set (Γ : Fml → Prop) : Theory signature :=
   { ψ | ∃ φ ∈ ℒₚₐ, Γ φ ∧ ψ = (disquotation_schema φ)}
 def tb : Theory signature := {φ | φ ∈ 𝐏𝐀𝐓 ∨ φ ∈ (disquotation_set Set.univ)}
 
-def eq_zero : Fml :=
-  ⊤
-#eval disquotation_schema eq_zero
-
 notation "𝐓𝐁" => tb
-
 end TB
 
 def dflt_f : SyntacticFormula signature := = ![&0,&0]
+
+def der_to_disquoted_list (d : Derivation 𝐓𝐁 Γ): (List Fml) :=
+  match d with
+  | .axL Δ r v => []
+  | .verum Δ => []
+  | .or der => der_to_disquoted_list der
+  | .and der₁ der₂ =>
+    if (der_to_disquoted_list der₁) ∩ (der_to_disquoted_list der₂) = ∅ then
+      (der_to_disquoted_list der₁) ++ (der_to_disquoted_list der₂) else
+      (der_to_disquoted_list der₁) ++ (List.diff (der_to_disquoted_list der₂) ((der_to_disquoted_list der₁) ∩ (der_to_disquoted_list der₂)))
+  | .all der => der_to_disquoted_list der
+  | .ex _ der => der_to_disquoted_list der
+  | .wk der _ => der_to_disquoted_list der
+  | .cut _ _ => []
+  | .root _ =>
+    match Γ with
+    | [φ] =>
+      match φ with
+      | Semiformula.and (Semiformula.or (Semiformula.nrel Rel.t v) (ψ₁)) (Semiformula.or (ψ₂) (Semiformula.rel Rel.t w)) =>
+        if ψ₁ = ∼ψ₂ ∧ v = w then [Semiformula.and (= ![&0,(v 0)]) ((Semiformula.ofNat 0 ((natural (v 0)).getD dflt)).getD dflt_f)] else []
+      | _ => []
+    | _ => []
+
+def build_tau_from_list : List Fml → Fml
+  | .nil => .verum
+  | .cons h Γ =>
+    h ⋎ (build_tau_from_list Γ)
+
+def list1 : List ℕ := [1,2,3]
+def list2 : List ℕ := [4,5]
+#eval list1 ∩ list2 = ∅
+
+def tau (der : Derivation 𝐓𝐁 Γ) : Fml :=
+  build_tau_from_list (der_to_disquoted_list der)
+
+def disq : Fml := TB.disquotation_schema ⊤
+def double_disq : Fml := disq ⋏ disq
+lemma disq_in_tb : disq ∈ 𝐓𝐁 := by
+  rw[disq,TB.tb]
+  simp
+  rw[TB.disquotation_set]
+  simp
+  let φ : Fml := ⊤
+  have step1 : φ ∈ ℒₚₐ := by
+    rw[lpa]
+    simp
+    simp[φ]
+    simp[contains_T]
+  have step2 : Set.univ φ := by
+    trivial
+  have step3 : TB.disquotation_schema ⊤ = TB.disquotation_schema φ := by
+    simp
+  have step4 : φ ∈ ℒₚₐ ∧ Set.univ φ ∧ TB.disquotation_schema ⊤ = TB.disquotation_schema φ := by
+    trivial
+  have step5 : ∃ φ ∈ ℒₚₐ, Set.univ φ ∧ TB.disquotation_schema ⊤ = TB.disquotation_schema φ := by
+    apply Exists.intro φ step4
+  apply Or.intro_right
+  trivial
+
+def der : Derivation 𝐓𝐁 [disq] :=
+  Derivation.root disq_in_tb
+
+#check der_to_disquoted_list der
+#eval der_to_disquoted_list der
+
+#check tau der
+#eval tau der
+
+def der_double_disq : Derivation 𝐓𝐁 [double_disq] := by
+  rw[double_disq]
+  apply Derivation.and
+  apply der
+  apply der
+
+#check der_to_disquoted_list der_double_disq
+#eval der_to_disquoted_list der_double_disq
+
+#check tau der_double_disq
+#eval tau der_double_disq
 
 -- one should match is up to a disquotation scheme enirely
 -- bewaren voor later: apply Semiformula.or (Semiformula.and (= ![&0,(v 0)]) ((Semiformula.ofNat 0 ((natural (v 0)).getD dflt)).getD dflt_f)) (tau_base_case Γ)
@@ -44,25 +118,24 @@ def tau_base_case : Sequent signature → SyntacticFormula signature :=
 
 def wo_t : Fml := = ![&0,&0]
 def w_t : Fml := T ![S ![zero]]
-def disq : Fml := TB.disquotation_schema ⊤
 def seq : Sequent signature := (wo_t :: [w_t,disq])
 
 #check Rewriting.fix (tau_base_case seq)
 def zero2 : Semiterm signature ℕ 1 := zero
 #eval (Rewriting.fix (tau_base_case seq))/[zero2]
 
-def tau : Derivation 𝐓𝐁 Γ → SyntacticFormula signature :=
-  fun der_tb : Derivation 𝐓𝐁 Γ =>
-    match der_tb with
-      | Derivation.axL Δ r v => tau_base_case Δ
-      | Derivation.verum Δ => tau_base_case Δ
-      | Derivation.or der => tau der
-      | Derivation.and der1 der2 => (tau der1) ⋎ (tau der2)
-      | Derivation.all der => tau der
-      | Derivation.ex _ der => tau der
-      | Derivation.wk der sub => tau der
-      | Derivation.cut der1 der2 => (tau der1) ⋎ (tau der2)
-      | Derivation.root _ => tau_base_case Γ
+-- def tau : Derivation 𝐓𝐁 Γ → SyntacticFormula signature :=
+--   fun der_tb : Derivation 𝐓𝐁 Γ =>
+--     match der_tb with
+--       | Derivation.axL Δ r v => tau_base_case Δ
+--       | Derivation.verum Δ => tau_base_case Δ
+--       | Derivation.or der => tau der
+--       | Derivation.and der1 der2 => (tau der1) ⋎ (tau der2)
+--       | Derivation.all der => tau der
+--       | Derivation.ex _ der => tau der
+--       | Derivation.wk der sub => tau der
+--       | Derivation.cut der1 der2 => (tau der1) ⋎ (tau der2)
+--       | Derivation.root _ => tau_base_case Γ
 
 def der_some_disq : Derivation 𝐓𝐁 [disq] := by
   have step1 : ⊤ ∈ ℒₜ := by
