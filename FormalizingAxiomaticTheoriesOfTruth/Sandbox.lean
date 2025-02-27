8f6f817e0169424741ea520f16a02a52910aa815
ppls-nd-prs <bramswaanen@gmail.com>
2025-02-27 17:01:06 +0100

    Added Sandbox.lean

---
+++ FormalizingAxiomaticTheoriesOfTruth/Sandbox.lean
@@ -0,0 +1,14 @@
+import Mathlib.ModelTheory.Basic
+import Mathlib.ModelTheory.Syntax
+import FormalizingAxiomaticTheoriesOfTruth.Prelims
+
+open FirstOrder
+open Language
+namespace LPATrying
+open Languages.LPA
+def zero_eq_zero : Formula ℒₚₐ ℕ :=
+  .equal (.func Func.zero ![]) (.func Func.zero ![])
+
+open Languages.L_T
+def szero_eq_zero : Formula ℒₜ ℕ :=
+  .equal (.func Func.zero ![]) (.func Func.zero ![])
