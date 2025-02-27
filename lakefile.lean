8168e639dcde2632deded9ff0f4b7d273b2aa8ba
ppls-nd-prs <bramswaanen@gmail.com>
2025-02-27 15:33:43 +0100

    Modified lakefile.lean: removed foundation as dependency.

--- lakefile.lean
+++ lakefile.lean
@@ -20,5 +20,3 @@ require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
 meta if get_config? env = some "dev" then
 require «doc-gen4» from git
   "https://github.com/leanprover/doc-gen4" @ "main"
-
-require foundation from git "https://github.com/FormalizedFormalLogic/Foundation.git" @ "d5a5b566c4bbc8d91a509979ae5091e750e67059"
