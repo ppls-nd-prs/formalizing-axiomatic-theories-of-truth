8bb2358ebe1f06380e9f59e9c193dd9db2321c66
ppls-nd-prs <bramswaanen@gmail.com>
2025-03-01 09:42:22 +0100

    Modified Prelims.lean: added own notations of formula, seems to work but I forsee trouble.

--- FormalizingAxiomaticTheoriesOfTruth/Prelims.lean
+++ FormalizingAxiomaticTheoriesOfTruth/Prelims.lean
@@ -6,7 +6,7 @@ open FirstOrder
 open Language
 
   namespace LPA
-    inductive Func : ℕ → Type u where
+    inductive Func : ℕ → Type _ where
       | zero : Func 0
       | succ : Func 1
       | add : Func 2
@@ -39,13 +39,13 @@ open Language
     open FirstOrder
     open Language
 
-    inductive Func : ℕ → Type u where
+    inductive Func : ℕ → Type _ where
       | zero : Func 0
       | succ : Func 1
       | add : Func 2
       | mult : Func 2
 
-    inductive Rel : ℕ → Type v where
+    inductive Rel : ℕ → Type _ where
       | t : Rel 1
 
     def signature : Language :=
@@ -57,6 +57,12 @@ open Language
     prefix:60 "T" => Formula.rel Rel.t
     notation "ℒₜ" => signature
   end L_T
+  /-- A term on `α` is either a variable indexed by an element of `α`
+    or a function symbol applied to simpler terms. -/
+  inductive Term (L : Language) (α : Type _) : Type _
+    | var : α → Term L α
+    | func : ∀ {l : ℕ} (_f : L.Functions l) (_ts : Fin l → Term L α), Term L α
+
   /-- `Formula α` is the type of formulas with free variables indexed by `α` -/
   inductive Formula (L: Language) : Type*
     | falsum : Formula L
@@ -64,6 +70,7 @@ open Language
     | rel {l : ℕ} (R : L.Relations l) (ts : Fin l → L.Term ℕ) : Formula L
     | imp (f₁ f₂ : Formula L) : Formula L
     | all (f : Formula L) : Formula L
+
   /-
   Some useful notation
   -/
@@ -84,20 +91,18 @@ open Language
     | .var α => .var α
     | .func (l := n) f v => .func (to_lt_func f) (fun i : Fin n => to_lt_t (v i))
 
-  def to_lt_bf: BoundedFormula ℒₚₐ ℕ n → BoundedFormula ℒₜ ℕ n
+  def to_lt_f: Formula ℒₚₐ → Formula ℒₜ
     | .falsum => .falsum
     | .equal t₁ t₂ => .equal (to_lt_t t₁) (to_lt_t t₂)
-    | .imp f₁ f₂ => .imp (to_lt_bf f₁) (to_lt_bf f₂)
-    | .all f => .all (to_lt_bf f)
+    | .imp f₁ f₂ => .imp (to_lt_f f₁) (to_lt_f f₂)
+    | .all f => .all (to_lt_f f)
 
-  def to_lt_f (f : Formula ℒₜ ℕ): Fml ℒₚₐ:=
-    to_lt_bf (f)
-  example {n : ℕ}: ∀φ:Formula LPA.signature ℕ, ∃ψ:Semiformula L_T.lt ℕ n, ψ = to_lt_f φ :=
-    fun a : Semiformula PA.lpa ℕ n => Exists.intro (to_lt_f a) (Eq.refl (to_lt_f a))
+  example : ∀φ:Formula ℒₚₐ, ∃ψ:Formula ℒₜ, ψ = to_lt_f φ :=
+    fun a : Formula ℒₚₐ => Exists.intro (to_lt_f a) (Eq.refl (to_lt_f a))
 
-  instance : Coe (Semiterm PA.lpa ℕ n) (Semiterm lt ℕ n) where
+  instance : Coe (Term ℒₚₐ ℕ) (Term ℒₜ ℕ) where
     coe t := to_lt_t t
-  instance : Coe (Semiformula PA.lpa ℕ n) (Semiformula lt ℕ n) where
+  instance : Coe (Formula ℒₚₐ) (Formula ℒₜ) where
     coe φ := to_lt_f φ
 
 
