1092f55db28626c6bcdc2377fc24712e84a70ed2
ppls-nd-prs <bramswaanen@gmail.com>
2025-03-05 12:43:38 +0100

    Modified Prelims.lean: started implementation of PA axioms.

--- FormalizingAxiomaticTheoriesOfTruth/Prelims.lean
+++ FormalizingAxiomaticTheoriesOfTruth/Prelims.lean
@@ -18,26 +18,23 @@ namespace Languages
     /-
     Useful notation
     -/
-    prefix:60 "S" => Term.func Func.succ
-    infix:60 "≃" => BoundedFormula.equal
-    prefix:60 "zero" => Term.func Func.zero
-    prefix:60 "add" => Term.func Func.add
-    prefix:60 "times" => Term.func Func.mult
+    notation "S(" n ")" => Term.func Func.succ ![n]
+    notation "zero" => Term.func Func.zero ![]
+    notation n "add" m => Term.func Func.add ![n,m]
+    notation n "times" m => Term.func Func.mult ![n,m]
     notation "ℒₚₐ" => signature
 
     /-
     Some useful terms
     -/
     def null : Term signature α :=
-      zero ![]
+      zero
     def numeral : ℕ → Term signature α
-      | .zero => zero ![]
-      | .succ n => S ![numeral n]
+      | .zero => zero
+      | .succ n => S(numeral n)
   end LPA
 
   namespace L_T
-    open FirstOrder
-    open Language
 
     inductive Func : ℕ → Type _ where
       | zero : Func 0
@@ -106,6 +103,37 @@ namespace Calculus
   | falsumE Γ A : prf (insert ∼A Γ) ⊥ → prf Γ A
   | allI Γ A : prf ((λf => f ↑ 1) '' Γ) A → prf Γ (∀' A)
   | allE₂ Γ A t : prf Γ (∀' A) → prf Γ (A/[t])
-  | ref Γ t : prf Γ (t ≃ t')
-  | subst₂ Γ s t f : prf Γ (s ≃ t) → prf Γ (f/[s]) → prf Γ (f/[t])
+  | ref Γ t : prf Γ (t =' t')
+  | subst₂ Γ s t f : prf Γ (s =' t) → prf Γ (f/[s]) → prf Γ (f/[t])
 end Calculus
+
+namespace PA
+  open Languages
+  open LPA
+  def succ_var_term : Term ℒₚₐ (ℕ ⊕ ℕ) :=
+    S(Term.var 0)
+  def eq_var : Formula ℒₚₐ ℕ :=
+    succ_var_term =' succ_var_term
+  #check eq_var/[LPA.null]
+  #check ∀' (succ_var_term =' succ_var_term)
+  inductive axioms : Theory ℒₚₐ where
+  | first : axioms (∀' ∼(LPA.null =' S(&0)))
+  | second :axioms (∀' ∀' ((S(&1) =' S(&0)) ⟹ (&1 =' &0)))
+  | third : axioms (∀' ((&0 add LPA.null) =' &0))
+  | fourth : axioms (∀' ∀' ((&1 add S(&0)) =' S(&1 add &0)))
+  | fifth : axioms (∀' ((&0 times LPA.null) =' LPA.null))
+  | sixth : axioms (∀' ∀' ((&1 times S(&0)) =' ((&1 times &0)) add &1))
+  | induction φ : (axioms (∼ (((φ/[LPA.null]) ⟹ ∼(∀'(φ ⟹ φ/[succ_var_term]))) ⟹ ∀' φ)))
+  /-
+  A coercion from ℒₚₐ Axioms to ℒₜ Axioms as all ℒₚₐ Axioms are also
+  ℒₜ Axioms -/
+  def
+  def to_lt_T : Theory ℒₚₐ → Theory ℒₜ := by
+    repeat rewrite[Theory]
+    repeat rewrite[Set]
+    intro set
+    intro φ
+    sorry
+  inductive axioms : Theory ℒₚₐ where
+  | first :
+end PA
