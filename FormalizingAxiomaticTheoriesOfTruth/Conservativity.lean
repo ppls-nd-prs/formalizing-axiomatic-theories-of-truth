92b685e98ac49e063baf7f6b8688a2783f55d33e
ppls-nd-prs <bramswaanen@gmail.com>
2025-05-14 16:17:53 +0200

    Added some stuff

--- FormalizingAxiomaticTheoriesOfTruth/Conservativity.lean
+++ FormalizingAxiomaticTheoriesOfTruth/Conservativity.lean
@@ -1,4 +1,23 @@
 import FormalizingAxiomaticTheoriesOfTruth.ProofTheory
+
+namespace PA
+  open FirstOrder
+  open Language
+  open L_T
+    /-- Peano arithemtic -/
+  inductive peano_arithmetic : Set (BoundedFormula ℒ ℕ n) where
+    | first : peano_arithmetic (∀' ∼(LPA.null =' S(&0)))
+    | second :peano_arithmetic (∀' ∀' ((S(&1) =' S(&0)) ⟹ (&1 =' &0)))
+    | third : peano_arithmetic (∀' ((&0 add LPA.null) =' &0))
+    | fourth : peano_arithmetic (∀' ∀' ((&1 add S(&0)) =' S(&1 add &0)))
+    | fifth : peano_arithmetic (∀' ((&0 times LPA.null) =' LPA.null))
+    | sixth : peano_arithmetic (∀' ∀' ((&1 times S(&0)) =' ((&1 times &0)) add &1))
+    | induction (φ) : peano_arithmetic (induction φ)
+
+  abbrev 𝐏𝐀 := peano_arithmetic
+
+end PA
+
 namespace Conservativity
 
   theorem conservativity_of_tb : ∀φ ∈ ℒ, 𝐓𝐁 ⊢ φ → 𝐏𝐀 ⊢ φ := by
--- FormalizingAxiomaticTheoriesOfTruth/Syntax.lean
+++ FormalizingAxiomaticTheoriesOfTruth/Syntax.lean
@@ -1,23 +1,23 @@
 import Mathlib.ModelTheory.Basic
 import Mathlib.ModelTheory.Syntax
 
-namespace Languages
-  namespace L_T
-    open FirstOrder
-    inductive Func : ℕ → Type _ where
-    | zero : Func 0
-    | succ : Func 1
-    | add : Func 2
-    | mult : Func 2
-    | neg : Func 1
-    | conj : Func 2
-    | disj : Func 2
-    | cond : Func 2
-    | forall : Func 1
-    | exists : Func 1
-    | denote : Func 1
-    | subs : Func 3
-    deriving DecidableEq
+namespace L_T
+  open FirstOrder
+  open Language
+  inductive Func : ℕ → Type _ where
+  | zero : Func 0
+  | succ : Func 1
+  | add : Func 2
+  | mult : Func 2
+  | neg : Func 1
+  | conj : Func 2
+  | disj : Func 2
+  | cond : Func 2
+  | forall : Func 1
+  | exists : Func 1
+  | denote : Func 1
+  | subs : Func 3
+  deriving DecidableEq
 
   inductive Rel : ℕ → Type _ where
     | var : Rel 1
@@ -34,6 +34,14 @@ namespace Languages
   def signature : Language :=
     ⟨Func, Rel⟩
 
+  /-
+  Useful notation
+  -/
+  scoped notation "S(" n ")" => Term.func Func.succ ![n]
+  scoped notation "zero" => Term.func Func.zero ![]
+  scoped notation n "add" m => Term.func Func.add ![n,m]
+  scoped notation n "times" m => Term.func Func.mult ![n,m]
+
   def ℒₜ {n} : Set (L_T.signature.BoundedFormula ℕ n) := Set.univ
 
     /-- Gives whether a BoundedFormula contains a T predicate-/
@@ -44,3 +52,4 @@ namespace Languages
   | _ => false
 
   def ℒ {n} : Set (L_T.signature.BoundedFormula ℕ n) := { φ | ¬contains_T φ}
+end L_T
