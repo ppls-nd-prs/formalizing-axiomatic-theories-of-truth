217dd8de619f474b62eb9ed9cc74c7bd7a6ad9ed
ppls-nd-prs <bramswaanen@gmail.com>
2025-06-15 20:28:05 +0200

    Modified Syntax; removed L_T namespace at top.

--- FormalizingAxiomaticTheoriesOfTruth/Syntax.lean
+++ FormalizingAxiomaticTheoriesOfTruth/Syntax.lean
@@ -1,38 +1,6 @@
 import Mathlib.ModelTheory.Basic
 import Mathlib.ModelTheory.Syntax
-
-namespace L_T
-  open FirstOrder
-  open Language
-  inductive Func : ℕ → Type _ where
-  | zero : Func 0
-  | succ : Func 1
-  | add : Func 2
-  | mult : Func 2
-  | neg : Func 1
-  | conj : Func 2
-  | disj : Func 2
-  | cond : Func 2
-  | forall : Func 1
-  | exists : Func 1
-  | denote : Func 1
-  | subs : Func 3
-  deriving DecidableEq
-
-  inductive Rel : ℕ → Type _ where
-    | var : Rel 1
-    | const : Rel 1
-    | t : Rel 1
-    | term : Rel 1
-    | clterm: Rel 1
-    | forml : Rel 1
-    | sentencel: Rel 1
-    | formlt : Rel 1
-    | sentencelt : Rel 1
-    deriving DecidableEq
-
-  def signature : Language :=
-    ⟨Func, Rel⟩
+import Mathlib.ModelTheory.Encoding 
 
 open FirstOrder
 open Language
